38c7b7ae053c319adb95ea39819a4021d0ab30eb
kwseekerli <kwseekerli@welove-inc.com>
2022-06-02 10:53:40 +0800

    JMH基准测试 init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Development Testing 
+
+开发需要掌握的测试技能 以及 开源的测试工具。
+
+## Testing Tech Stack
+
+## OpenSource Tools
+
++ [JMH](https://github.com/openjdk/jmh) (Java Microbenchmark Harness)
---
+++ jmh-usage/src/test/java/top/kwseeker/testing/jmh/JMHTest.java
@@ -0,0 +1,15 @@
+package top.kwseeker.testing.jmh;
+
+import org.junit.Test;
+
+/**
+ * 官方基准测试使用案例：
+ * https://github.com/openjdk/jmh jmh-samples
+ */
+public class JMHTest {
+
+    @Test
+    public void test() {
+
+    }
+}
